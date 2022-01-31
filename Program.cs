4c92ef8b1fc601c4012d8fd287609b952daed865
River Diniz <river-s-diniz@hotmail.com>
2022-01-30 21:21:17 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using dotnet_poo.src.Entities;
+
+namespace dotnet_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+           
+            Console.WriteLine(wizard.Attack(1));
+        }
+    }
+}
